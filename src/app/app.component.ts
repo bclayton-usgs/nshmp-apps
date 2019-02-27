c8f972c2dc41d8e28b954e4c44ea04ead2fef351
bclayton-usgs <bclayton@usgs.gov>
2019-02-26 17:01:46 -0700

    Add nshmp-ng-template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Navigation } from '@nshmp/nshmp-ng-template';
+
+import { NavigationListService } from './shared/navigation-list.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'nshmp-haz-web-apps';
+
+  navigationList: Navigation[];
+
+  constructor(private navListService: NavigationListService) {}
+
+  ngOnInit() {
+    this.navigationList = this.navListService.getNavList();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NshmpTemplateModule } from '@nshmp/nshmp-ng-template';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NshmpTemplateModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
