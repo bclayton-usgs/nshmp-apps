4c92dd79a0b97f6562959503230d69e801bb09cd
bclayton-usgs <bclayton@usgs.gov>
2019-03-06 16:24:32 -0700

    Moved Angular mateial to own module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {
-  MatCardModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatButtonModule,
-  MatSlideToggleModule,
-  MatTooltipModule
-} from '@angular/material';
-
-import { ReactiveFormsModule } from '@angular/forms';
+import { NshmpTemplateModule } from '@nshmp/nshmp-ng-template';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NshmpTemplateModule } from '@nshmp/nshmp-ng-template';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ExceedanceExplorerComponent } from './exceedance-explorer/exceedance-explorer.component';
-import { FormControlsComponent } from './form-controls/form-controls.component';
-import { ExceedanceControlPanelComponent } from './exceedance-explorer/exceedance-control-panel/exceedance-control-panel.component';
-import { ExceedancePlotComponent } from './exceedance-explorer/exceedance-plot/exceedance-plot.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
-    ExceedanceExplorerComponent,
-    FormControlsComponent,
-    ExceedanceControlPanelComponent,
-    ExceedancePlotComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    CoreModule,
     NshmpTemplateModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatInputModule,
-    ReactiveFormsModule,
-    MatButtonModule,
-    MatSlideToggleModule,
-    MatTooltipModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
