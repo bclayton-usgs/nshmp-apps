0a2cc5abdcd01484673dfee0456cfb2bcc88df12
bclayton-usgs <bclayton@usgs.gov>
2019-03-07 09:39:36 -0700

    Title in browser tab updates based on router link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Navigation, NavigationService } from '@nshmp/nshmp-ng-template';
+import { Router } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'nshmp-apps';
+export class AppComponent implements OnInit, OnDestroy {
+
+  routerSubscription: Subscription;
 
   navigationList: Navigation[] = [
     {
@@ -19,10 +23,29 @@ export class AppComponent implements OnInit {
     }
   ];
 
-  constructor(private navigationService: NavigationService) {}
+  constructor(
+    private navigationService: NavigationService,
+    private router: Router,
+    private titleService: Title) {}
 
   ngOnInit() {
     this.navigationService.set(this.navigationList);
+
+    this.routerSubscription = this.router.events.subscribe(() => {
+      this.setTitle();
+    });
+  }
+
+  ngOnDestroy() {
+    this.routerSubscription.unsubscribe();
+  }
+
+  private setTitle() {
+    const currentRoute = this.navigationList.find(nav => {
+      return `/${nav.routerLink}` === this.router.url;
+    });
+
+    this.titleService.setTitle(currentRoute.display);
   }
 
 }
