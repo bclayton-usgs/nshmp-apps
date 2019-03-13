597bb30245854c0be06ac2ac8f734f709ae17304
bclayton-usgs <bclayton@usgs.gov>
2019-03-13 09:22:29 -0600

    added gmm select menu

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,26 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { NshmpControlPanelModule } from '@nshmp/nshmp-ng-template';
 
 import { FormControlsComponent } from '../form-controls/form-controls.component';
 import { MaterialModule } from './material.module';
+import { GmmMenuComponent } from '../gmm/gmm-menu/gmm-menu.component';
 
 @NgModule({
   declarations: [
     FormControlsComponent,
+    GmmMenuComponent,
   ],
   imports: [
     CommonModule,
     MaterialModule,
     NshmpControlPanelModule,
     ReactiveFormsModule,
+    HttpClientModule,
   ],
   exports: [
     FormControlsComponent,
+    GmmMenuComponent,
     MaterialModule,
     NshmpControlPanelModule,
     ReactiveFormsModule,
+    HttpClientModule
   ]
 })
 export class CoreModule { }
