e4cf56b47d9beb7161e4ce888ad44af648ad8ced
bclayton-usgs <bclayton@usgs.gov>
2019-06-05 08:57:01 -0600

    hazard map plot component

---
+++ src/app/hazard/hazard-map/hazard-map-plot/hazard-map-plot.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hazard-map-plot',
+  templateUrl: './hazard-map-plot.component.html',
+  styleUrls: ['./hazard-map-plot.component.scss']
+})
+export class HazardMapPlotComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
